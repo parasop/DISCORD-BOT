2ec8ee4289518329ad9c86fb0516922908848da9
parasop <63485824+parasop@users.noreply.github.com>
2022-09-27 19:24:53 +0530

    Update addbadge.js

--- commands/DEVELOPER/addbadge.js
+++ commands/DEVELOPER/addbadge.js
@@ -11,8 +11,7 @@ run: async (client, message, args) => {
     
         let member = message.mentions.members.first()|| message.guild.members.cache.get(args[0]);
     
-        let Member = await Data.findOne({ memberID: member.user.id });
-        if (!Member) { Member =  await Data.create({ memberID: member.user.id }); }
+        let Member = await Data.findOne({ userID: member.user.id })|| new Data({ userID: member.user.id }); 
         
         if(!args[1]){
         
